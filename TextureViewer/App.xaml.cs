f568531f498b25287ad8fa710155556cde48abc3
Felix Brüll <felixbruell@live.de>
2017-07-23 22:23:03 +0200

    initial commit

---
+++ TextureViewer/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TextureViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var window = new MainWindow(e.Args);
+            window.Show();
+            
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
