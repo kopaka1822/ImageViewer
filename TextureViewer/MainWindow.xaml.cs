34aeee02ae4f687fd3512e9048bcc67f021caf9a
Felix Brüll <felixbruell@live.de>
2017-07-24 17:23:20 +0200

    opengl will draw colored quad

--- TextureViewer/MainWindow.xaml.cs
+++ TextureViewer/MainWindow.xaml.cs
@@ -22,8 +22,6 @@ namespace TextureViewer
     /// </summary>
     public partial class MainWindow : Window
     {
-        float rotatePyramid = 0;
-        float rquad = 0;
 
         public MainWindow(string file)
         {
@@ -36,108 +34,37 @@ namespace TextureViewer
             //  Get the OpenGL instance that's been passed to us.
             OpenGL gl = args.OpenGL;
 
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();
+
             //  Clear the color and depth buffers.
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
 
             //  Reset the modelview matrix.
             gl.LoadIdentity();
 
-            //  Move the geometry into a fairly central position.
-            gl.Translate(-1.5f, 0.0f, -6.0f);
-
-            //  Draw a pyramid. First, rotate the modelview matrix.
-            gl.Rotate(rotatePyramid, 0.0f, 1.0f, 0.0f);
-
             //  Start drawing triangles.
-            gl.Begin(OpenGL.GL_TRIANGLES);
-
+            gl.Begin(OpenGL.GL_TRIANGLE_STRIP);
+            
             gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-
-            gl.End();
-
-            //  Reset the modelview.
-            gl.LoadIdentity();
-
-            //  Move into a more central position.
-            gl.Translate(1.5f, 0.0f, -7.0f);
-
-            //  Rotate the cube.
-            gl.Rotate(rquad, 1.0f, 1.0f, 1.0f);
-
-            //  Provide the cube colors and geometry.
-            gl.Begin(OpenGL.GL_QUADS);
-
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, 1.0f, -1.0f);
-            gl.Vertex(-1.0f, 1.0f, -1.0f);
-            gl.Vertex(-1.0f, 1.0f, 1.0f);
-            gl.Vertex(1.0f, 1.0f, 1.0f);
-
-            gl.Color(1.0f, 0.5f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(1.0f, 1.0f, 1.0f);
-            gl.Vertex(-1.0f, 1.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-
-            gl.Color(1.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-            gl.Vertex(-1.0f, 1.0f, -1.0f);
-            gl.Vertex(1.0f, 1.0f, -1.0f);
-
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, 1.0f, 1.0f);
-            gl.Vertex(-1.0f, 1.0f, -1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-
-            gl.Color(1.0f, 0.0f, 1.0f);
-            gl.Vertex(1.0f, 1.0f, -1.0f);
-            gl.Vertex(1.0f, 1.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, 1.0f, 0.0f);
 
             gl.End();
 
             //  Flush OpenGL.
             gl.Flush();
+        }
 
-            //  Rotate the geometry a bit.
-            rotatePyramid += 3.0f;
-            rquad -= 3.0f;
-
+        private void OpenGLControl_OnOpenGLInitialized(object sender, OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+            
         }
     }
 }
