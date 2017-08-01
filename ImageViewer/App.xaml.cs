ec596a42d9a3a3a62adba691f81c380aa5db68f3
Felix Brüll <felixbruell@live.de>
2017-08-01 12:47:41 +0200

    added new project which supports opengl 4

---
+++ ImageViewer/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainWindow win1;
+        private MainWindow win2;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            win1 = new MainWindow();
+            win1.Show();
+            win2 = new MainWindow();
+            win2.Show();
+        }
+    }
+}
---
+++ ImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK.WPF;
+
+using OpenTK.Graphics.OpenGL4;
+using ClearBufferMask = OpenTK.Graphics.OpenGL4.ClearBufferMask;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static float r = 0.0f;
+        private float red;
+        public MainWindow()
+        {
+            InitializeComponent();
+            red = r;
+            r += 1.0f;
+        }
+
+        private void OtkWpfControl_OnInitialized(object sender, EventArgs e)
+        {
+        }
+
+        private void OtkWpfControl_OnResized(object sender, EventArgs e)
+        {
+        }
+
+        private void OtkWpfControl_OnOpenGLDraw(object sender, OtkWpfControl.OpenGLDrawEventArgs e)
+        {
+            var ctrl = sender as OpenTK.WPF.OtkWpfControl;
+            if (ctrl != null)
+            {
+                GL.ClearColor(red,red,red, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                
+                e.Redrawn = true;
+            }
+        }
+    }
+}
