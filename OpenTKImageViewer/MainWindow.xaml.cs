f6340529f298efcac9a7cb2f081f837f2270720e
Felix Brüll <felixbruell@live.de>
2017-08-08 15:31:12 +0200

    added menu (mostly not funtional)

--- OpenTKImageViewer/MainWindow.xaml.cs
+++ OpenTKImageViewer/MainWindow.xaml.cs
@@ -118,6 +118,14 @@ namespace OpenTKImageViewer
             }
         }
 
+        /// <summary>
+        /// the frame will be redrawn as soon as possible
+        /// </summary>
+        public void RedrawFrame()
+        {
+            glControl?.Invalidate();
+        }
+
         private void InitGraphics()
         { 
             GL.Enable(EnableCap.TextureCubeMapSeamless);
@@ -153,12 +161,11 @@ namespace OpenTKImageViewer
                     error = exception.Message + ": " + exception.StackTrace;
             }
 
-            glControl.Invalidate();
         }
 
         private void WinFormsHost_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-
+            RedrawFrame();
         }
 
         #region WINDOW INTERACTION
@@ -176,7 +183,10 @@ namespace OpenTKImageViewer
                 var diff = newPosition - mousePosition;
 
                 if (Math.Abs(diff.X) > 0.01 || Math.Abs(diff.Y) > 0.01)
+                {
                     imageViews[currentImageView]?.OnDrag(diff, this);
+                    RedrawFrame();
+                }
             }
             mousePosition = newPosition;
         }
@@ -201,6 +211,7 @@ namespace OpenTKImageViewer
         private void WinFormsHost_OnMouseWheel(System.Windows.Forms.MouseEventArgs args)
         {
             imageViews[currentImageView]?.OnScroll(args.Delta, new Point(args.X, args.Y));
+            RedrawFrame();
         }
         
         #endregion
@@ -232,6 +243,120 @@ namespace OpenTKImageViewer
                     Context.ActiveMipmap += 1;
                     break;
             }
+            RedrawFrame();
         }
+
+        #region MENU ITEMS
+
+        private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == true)
+            {
+                if (Context.GetNumImages() == 0)
+                {
+                    // TODO reinit window instead of closing
+                    parent.SpawnWindow(ofd.FileName);
+                    this.Close();
+                }
+                else
+                {
+                    parent.SpawnWindow(ofd.FileName);
+                }
+            }
+        }
+
+        private void MenuItem_OnChecked_LinearFiltering(object sender, RoutedEventArgs e)
+        {
+            Context.LinearInterpolation = MenuItemLinearInterpolation.IsChecked;
+        }
+
+        private void MenuItem_OnChecked_Grayscale(object sender, RoutedEventArgs e)
+        {
+            UpdateGrayscale(e.Source);
+        }
+
+        private void UpdateGrayscale(object sender)
+        {
+            MenuItemGrayscaleDisabled.IsCheckable = true;
+            MenuItemGrayscaleRed.IsCheckable = true;
+            MenuItemGrayscaleGreen.IsCheckable = true;
+            MenuItemGrayscaleBlue.IsCheckable = true;
+            MenuItemGrayscaleAlpha.IsCheckable = true;
+
+            // Determine which was checked.
+            if (Equals(sender, MenuItemGrayscaleDisabled))
+            {
+                MenuItemGrayscaleDisabled.IsCheckable = false;
+                MenuItemGrayscaleRed.IsChecked = false;
+                MenuItemGrayscaleGreen.IsChecked = false;
+                MenuItemGrayscaleBlue.IsChecked = false;
+                MenuItemGrayscaleAlpha.IsChecked = false;
+            }
+            else if (Equals(sender, MenuItemGrayscaleRed))
+            {
+                MenuItemGrayscaleRed.IsCheckable = false;
+                MenuItemGrayscaleDisabled.IsChecked = false;
+                MenuItemGrayscaleGreen.IsChecked = false;
+                MenuItemGrayscaleBlue.IsChecked = false;
+                MenuItemGrayscaleAlpha.IsChecked = false;
+            }
+            else if (Equals(sender, MenuItemGrayscaleGreen))
+            {
+                MenuItemGrayscaleGreen.IsCheckable = false;
+                MenuItemGrayscaleRed.IsChecked = false;
+                MenuItemGrayscaleDisabled.IsChecked = false;
+                MenuItemGrayscaleBlue.IsChecked = false;
+                MenuItemGrayscaleAlpha.IsChecked = false;
+            }
+            else if (Equals(sender, MenuItemGrayscaleBlue))
+            {
+                MenuItemGrayscaleBlue.IsCheckable = false;
+                MenuItemGrayscaleRed.IsChecked = false;
+                MenuItemGrayscaleGreen.IsChecked = false;
+                MenuItemGrayscaleDisabled.IsChecked = false;
+                MenuItemGrayscaleAlpha.IsChecked = false;
+            }
+            else if (Equals(sender, MenuItemGrayscaleAlpha))
+            {
+                MenuItemGrayscaleAlpha.IsCheckable = false;
+                MenuItemGrayscaleRed.IsChecked = false;
+                MenuItemGrayscaleGreen.IsChecked = false;
+                MenuItemGrayscaleBlue.IsChecked = false;
+                MenuItemGrayscaleDisabled.IsChecked = false;
+            }
+
+            if (MenuItemGrayscaleDisabled.IsChecked)
+                Context.Grayscale = ImageContext.ImageContext.GrayscaleMode.Disabled;
+            else if (MenuItemGrayscaleRed.IsChecked)
+                Context.Grayscale = ImageContext.ImageContext.GrayscaleMode.Red;
+            else if (MenuItemGrayscaleGreen.IsChecked)
+                Context.Grayscale = ImageContext.ImageContext.GrayscaleMode.Green;
+            else if (MenuItemGrayscaleBlue.IsChecked)
+                Context.Grayscale = ImageContext.ImageContext.GrayscaleMode.Blue;
+            else if (MenuItemGrayscaleAlpha.IsChecked)
+                Context.Grayscale = ImageContext.ImageContext.GrayscaleMode.Alpha;
+        }
+
+        private void MenuItem_Click_Mipmaps(object sender, RoutedEventArgs e)
+        {
+            parent.OpenDialog(App.UniqueDialog.Mipmaps);
+        }
+
+        private void MenuItem_Click_Layers(object sender, RoutedEventArgs e)
+        {
+            parent.OpenDialog(App.UniqueDialog.Layer);
+        }
+
+        private void MenuItem_Click_Images(object sender, RoutedEventArgs e)
+        {
+            parent.OpenDialog(App.UniqueDialog.Image);
+        }
+
+
+
+        #endregion
     }
 }
