ff36aa08fead067f8f6fc09227856df23d93f39b
Felix <felixbruell@live.de>
2018-05-03 15:08:15 +0200

    added sampler to gl model

--- TextureViewer/Models/OpenGlModel.cs
+++ TextureViewer/Models/OpenGlModel.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
 using TextureViewer.Controller;
 using TextureViewer.Controller.TextureViews.Shader;
 using TextureViewer.glhelper;
@@ -18,18 +19,54 @@ namespace TextureViewer.Models
         public VertexArray Vao { get; }
         public CheckersShader CheckersShader { get; }
 
+        private Sampler samplerLinear;
+        private Sampler samplerLinearMip;
+        private Sampler samplerNearest;
+        private Sampler samplerNearestMip;
 
         public OpenGlModel(OpenGlController controller)
         {
             Debug.Assert(controller.IsEnabled);
             Vao = new VertexArray();
             CheckersShader = new CheckersShader();
+            samplerLinear = new Sampler(TextureMinFilter.Linear, TextureMagFilter.Linear);
+            samplerLinearMip = new Sampler(TextureMinFilter.LinearMipmapLinear, TextureMagFilter.Linear);
+            samplerNearest = new Sampler(TextureMinFilter.Nearest, TextureMagFilter.Nearest);
+            samplerNearestMip = new Sampler(TextureMinFilter.NearestMipmapNearest, TextureMagFilter.Nearest);
+        }
+
+        /// <summary>
+        /// binds a specific sampler
+        /// </summary>
+        /// <param name="unit">sampler binding point</param>
+        /// <param name="hasMipmaps">mipmap sampling enabled</param>
+        /// <param name="linear">linear interpolation enabled</param>
+        public void BindSampler(int unit, bool hasMipmaps, bool linear)
+        {
+            if (hasMipmaps)
+            {
+                if (linear)
+                    samplerLinearMip.Bind(unit);
+                else
+                    samplerNearestMip.Bind(unit);
+            }
+            else
+            {
+                if (linear)
+                    samplerLinear.Bind(unit);
+                else
+                    samplerNearest.Bind(unit);
+            }
         }
 
         public void Dispose()
         {
             CheckersShader.Dispose();
             Vao.Dispose();
+            samplerLinear.Dispose();
+            samplerLinearMip.Dispose();
+            samplerNearest.Dispose();
+            samplerNearestMip.Dispose();
         }
     }
 }
