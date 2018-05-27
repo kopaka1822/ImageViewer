6731a131f8707e35053209a5dcad46ed5acdf1f6
Felix Brüll <felixbruell@live.de>
2018-05-27 23:09:26 +0200

    fixed clear color

--- TextureViewer/Controller/PaintController.cs
+++ TextureViewer/Controller/PaintController.cs
@@ -58,7 +58,7 @@ namespace TextureViewer.Controller
                     // draw the frame
 
                     GL.Viewport(0, 0, context.ClientSize.Width, context.ClientSize.Height);
-                    GL.ClearColor(0.9333f, 0.9333f, 0.9333f, 1.0f);
+                    GL.ClearColor(0.854992608124234f, 0.854992608124234f, 0.854992608124234f, 1.0f);
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
                     //models.GlData.CheckersShader.Bind(Matrix4.Identity);
