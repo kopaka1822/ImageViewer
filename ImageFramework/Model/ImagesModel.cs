ee7052fdd4390cd05736b3a4390612f147bd906d
Felix Brüll <felixbruell@live.de>
2019-09-28 23:22:53 +0200

    modified images model

--- ImageFramework/Model/ImagesModel.cs
+++ ImageFramework/Model/ImagesModel.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ImageFramework.Annotations;
 using ImageFramework.DirectX;
+using ImageFramework.ImageLoader;
+using SharpDX.DXGI;
 
 namespace ImageFramework.Model
 {
-    public class ImagesModel : INotifyPropertyChanged
+    public class ImagesModel : INotifyPropertyChanged, IDisposable
     { 
         private struct Dimension
         {
@@ -20,45 +23,45 @@ namespace ImageFramework.Model
 
         private Dimension[] dimensions = null;
 
-        public class TextureArray2DInfo
+        public class MipmapMismatch : Exception
         {
-            public TextureArray2D Image { get; set; }
-            public string Filename { get; set; }
-            public bool IsGrayscale { get; set; }
-            public bool IsAlpha { get; set; }
+            public MipmapMismatch(string message) : base(message)
+            {}
         }
 
-        private class ImageData : IDisposable
+        public class ImageData : IDisposable
         {
             public TextureArray2D Image { get; private set; }
             public int NumMipmaps => Image.NumMipmaps;
             public int NumLayers => Image.NumLayers;
             public bool IsGrayscale { get; }
             public bool HasAlpha { get; }
-            public bool IsHdr { get; }
+            public bool IsHdr => Image.Format == Format.R32G32B32A32_Float;
             public string Filename { get; }
-            public string FormatName { get; }
+            public GliFormat OriginalFormat { get; }
 
-            public ImageData(TextureArray2DInfo info)
+            internal ImageData(TextureArray2D image, string filename, GliFormat originalFormat)
             {
-                Image = info.Image;
-                IsGrayscale = info.IsGrayscale;
-                HasAlpha = info.IsAlpha;
-                IsHdr = true;
-                Filename = info.Filename;
-                //FormatName = 
-                // TODO format name
-                throw new NotImplementedException();
+                Image = image;
+                // TODO determine alpha and grayscale by looking at image content
+                IsGrayscale = false;
+                HasAlpha = true;
+                Filename = filename;
+                OriginalFormat = originalFormat;
             }
 
-            public void GenerateMipmaps(int levels)
+            internal void GenerateMipmaps(int levels)
             {
-                throw new NotImplementedException();
+                var tmp = Image.GenerateMipmapLevels(levels);
+                Image.Dispose();
+                Image = tmp;
             }
 
-            public void DeleteMipmaps()
+            internal void DeleteMipmaps()
             {
-                throw new NotImplementedException();
+                var tmp = Image.CloneWithoutMipmaps();
+                Image.Dispose();
+                tmp = Image;
             }
 
 
@@ -70,12 +73,268 @@ namespace ImageFramework.Model
 
         private readonly List<ImageData> images = new List<ImageData>();
 
+        #region Public Properties
+
+        public IReadOnlyList<ImageData> Images { get; }
+
+        // this property change will be triggered if the image order changes (and not if the number of images changes)
+        public static string ImageOrder = nameof(ImageOrder);
+        public int NumImages => images.Count;
+        public int NumMipmaps => images.Count == 0 ? 0 : images[0].NumMipmaps;
+        public int NumLayers => images.Count == 0 ? 0 : images[0].NumLayers;
+        /// <summary>
+        /// true if all images are grayscale
+        /// </summary>
+        public bool IsGrayscale => images.All(imageData => imageData.IsGrayscale);
+        /// <summary>
+        /// true if any image has an alpha channel
+        /// </summary>
+        public bool IsAlpha => images.Any(imageData => imageData.HasAlpha);
+        /// <summary>
+        /// true if any image is hdr
+        /// </summary>
+        public bool IsHdr => images.Any(imageData => imageData.IsHdr);
+
+        /// <summary>
+        /// width for the biggest mipmap (or 0 if no images are present)
+        /// </summary>
+        public int Width => images.Count != 0 ? GetWidth(0) : 0;
+        /// <summary>
+        /// height for the biggest mipmap (or 0 if no images are present)
+        /// </summary>
+        public int Height => images.Count != 0 ? GetHeight(0) : 0;
+
+        // helper for many other models
+        /// <summary>
+        /// previous number of images
+        /// </summary>
+        public int PrevNumImages { get; private set; } = 0;
+
+        /// <summary>
+        /// width of the mipmap
+        /// </summary>
+        /// <param name="mipmap"></param>
+        /// <returns></returns>
+        public int GetWidth(int mipmap)
+        {
+            Debug.Assert(images.Count != 0);
+            Debug.Assert(mipmap < NumMipmaps && mipmap >= 0);
+            return dimensions[mipmap].Width;
+        }
+
+        /// <summary>
+        /// height of the mipmap
+        /// </summary>
+        /// <param name="mipmap"></param>
+        /// <returns></returns>
+        public int GetHeight(int mipmap)
+        {
+            Debug.Assert(images.Count != 0);
+            Debug.Assert(mipmap < NumMipmaps && mipmap >= 0);
+            return dimensions[mipmap].Height;
+        }
+
+        #endregion
+
+        public ImagesModel()
+        {
+            Images = images;
+        }
+
+        /// <summary>
+        /// tries to add the image to the current collection.
+        /// </summary>
+        /// <exception cref="MipmapMismatch">will be thrown if everything except the number of mipmaps matches</exception>
+        /// <exception cref="Exception">will be thrown if the images does not match with the current set of images</exception>
+        public void AddImage(TextureArray2D image, string name, GliFormat originalFormat)
+        {
+            if (images.Count == 0) // first image
+            {
+                InitDimensions(image);
+                images.Add(new ImageData(image, name, originalFormat));
+                OnPropertyChanged(nameof(NumImages));
+                OnPropertyChanged(nameof(NumLayers));
+                OnPropertyChanged(nameof(NumMipmaps));
+                OnPropertyChanged(nameof(IsAlpha));
+                OnPropertyChanged(nameof(IsGrayscale));
+                OnPropertyChanged(nameof(IsHdr));
+                OnPropertyChanged(nameof(Width));
+                OnPropertyChanged(nameof(Height));
+            }
+            else // test if compatible with previous images
+            {
+                if(image.NumLayers != NumLayers)
+                    throw new Exception($"{name}: Inconsistent amount of layers. Expected {NumLayers} got {image.NumLayers}");
+
+                if (image.GetWidth(0) != GetWidth(0) || image.GetHeight(0) != GetHeight(0))
+                    throw new Exception($"{name}: Image resolution mismatch. Expected {GetWidth(0)}x{GetHeight(0)} but got {image.GetWidth(0)}x{image.GetHeight(0)}");
+
+                if (image.NumMipmaps != NumMipmaps)
+                    throw new MipmapMismatch($"{name}: Inconsistent amount of mipmaps. Expected {NumMipmaps} got {image.NumMipmaps}");
+
+                // remember old properties
+                var isAlpha = IsAlpha;
+                var isGrayscale = IsGrayscale;
+                var isHdr = IsHdr;
+
+                images.Add(new ImageData(image, name, originalFormat));
+                PrevNumImages = NumImages - 1;
+                OnPropertyChanged(nameof(NumImages));
+
+                if (isAlpha != IsAlpha)
+                    OnPropertyChanged(nameof(isAlpha));
+                if (isGrayscale != IsGrayscale)
+                    OnPropertyChanged(nameof(IsGrayscale));
+                if (isHdr != IsHdr)
+                    OnPropertyChanged(nameof(IsHdr));
+            }
+        }
+
+        public void DeleteImage(int imageId)
+        {
+            Debug.Assert(imageId >= 0 && imageId < NumImages);
+
+            // remember old properties
+            var isAlpha = IsAlpha;
+            var isGrayscale = IsGrayscale;
+            var isHdr = IsHdr;
+
+            // delete old data
+            images[imageId].Dispose();
+            images.RemoveAt(imageId);
+
+            PrevNumImages = NumImages + 1;
+            OnPropertyChanged(nameof(NumImages));
+
+            if (isAlpha != IsAlpha)
+                OnPropertyChanged(nameof(isAlpha));
+            if (isGrayscale != IsGrayscale)
+                OnPropertyChanged(nameof(IsGrayscale));
+            if (isHdr != IsHdr)
+                OnPropertyChanged(nameof(IsHdr));
+
+            if (NumImages == 0)
+            {
+                // everything was resettet
+                OnPropertyChanged(nameof(NumLayers));
+                OnPropertyChanged(nameof(NumMipmaps));
+                OnPropertyChanged(nameof(Width));
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+
+        /// <summary>
+        /// moves the image from index 1 to index 2
+        /// </summary>
+        /// <param name="idx1">current image index</param>
+        /// <param name="idx2">index after moving the image</param>
+        public void MoveImage(int idx1, int idx2)
+        {
+            Debug.Assert(idx1 >= 0);
+            Debug.Assert(idx2 >= 0);
+            Debug.Assert(idx1 < NumImages);
+            Debug.Assert(idx2 < NumImages);
+            if (idx1 == idx2) return;
+
+            var i = images[idx1];
+            images.RemoveAt(idx1);
+            images.Insert(idx2, i);
+
+            OnPropertyChanged(nameof(ImageOrder));
+        }
+
+        /// <summary>
+        /// generates mipmaps for all images
+        /// </summary>
+        public void GenerateMipmaps()
+        {
+            Debug.Assert(NumMipmaps == 1);
+
+            // compute new mipmap levels
+            var levels = ComputeMaxMipLevels();
+            if (levels == NumMipmaps) return;
+
+            foreach (var image in images)
+            {
+                image.GenerateMipmaps(levels);
+            }
+
+            // recalc dimensions array
+            var w = Width;
+            var h = Height;
+            dimensions = new Dimension[levels];
+            for (int i = 0; i < levels; ++i)
+            {
+                dimensions[i].Width = w;
+                dimensions[i].Height = h;
+                w = Math.Max(w / 2, 1);
+                h = Math.Max(h / 2, 1);
+            }
+
+            OnPropertyChanged(nameof(NumMipmaps));
+        }
+
+        public void DeleteMipmaps()
+        {
+            Debug.Assert(NumMipmaps > 1);
+            foreach (var image in images)
+            {
+                image.DeleteMipmaps();
+            }
+            // refresh dimensions array
+            var w = Width;
+            var h = Height;
+            dimensions = new Dimension[1];
+            dimensions[0].Width = w;
+            dimensions[0].Height = h;
+
+            OnPropertyChanged(nameof(NumMipmaps));
+        }
+
+        private void InitDimensions(TextureArray2D image)
+        {
+            dimensions = new Dimension[image.NumMipmaps];
+            for (var i = 0; i < image.NumMipmaps; ++i)
+            {
+                dimensions[i] = new Dimension
+                {
+                    Width = image.GetWidth(i),
+                    Height = image.GetHeight(i)
+                };
+            }
+        }
+
+        /// <summary>
+        /// computes the maximum amount of mipmap levels for the current width and height
+        /// </summary>
+        /// <returns></returns>
+        private int ComputeMaxMipLevels()
+        {
+            var resolution = Math.Max(Width, Height);
+            var maxMip = 1;
+            while ((resolution /= 2) > 0) ++maxMip;
+            return maxMip;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// deletes all opengl related data.
+        /// Component should not be used after this
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var imageData in images)
+            {
+                imageData.Dispose();
+            }
+            images.Clear();
         }
     }
 }
