bb2b0925ac0519457daa1af8e84439d209eae872
Felix Brüll <felixbruell@live.de>
2019-10-03 00:07:01 +0200

    added console project

---
+++ ImageConsole/Commands/Command.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ImageFramework.Model;
+
+namespace ImageConsole.Commands
+{
+    public abstract class Command
+    {
+        protected Command(string code, string arguments, string description)
+        {
+            Code = code;
+            Arguments = arguments;
+            Description = description;
+        }
+
+        /// <summary>
+        /// command code: -open, -export (including the minus)
+        /// </summary>
+        public string Code { get; }
+
+        /// <summary>
+        /// list of expected arguments
+        /// </summary>
+        public string Arguments { get; }
+
+        /// <summary>
+        /// what does the command do, what are the arguments
+        /// </summary>
+        public string Description { get; }
+
+        public abstract void Execute(List<string> arguments, Models model);
+    }
+}
---
+++ ImageConsole/Commands/OpenCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageFramework.Model;
+
+namespace ImageConsole.Commands
+{
+    public class OpenCommand : Command
+    {
+        public OpenCommand()
+            :
+            base("-open", "\"file1\" \"file2\" ...","imports all filenames as images")
+        {}
+
+        public override void Execute(List<string> arguments, Models model)
+        {
+            foreach (var filename in arguments)
+            {
+                model.AddImageFromFile(filename);
+            }
+        }
+    }
+}
