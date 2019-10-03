d3563cd09cf08756fdb0f53fba235391e89b9f8b
Felix Brüll <felixbruell@live.de>
2019-10-03 16:08:07 +0200

    started with filter shader

---
+++ ImageFramework/Model/Shader/FilterShader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageFramework.Model.Filter;
+
+namespace ImageFramework.Model.Shader
+{
+    public class FilterShader : IDisposable
+    {
+        private readonly FilterModel parent;
+
+        public FilterShader(FilterModel parent, string source)
+        {
+            this.parent = parent;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
