6581434a56b70858cc88067775acd281c05cb420
Felix Brüll <felixbruell@live.de>
2019-10-05 19:59:46 +0200

    more ui stuff

---
+++ ImageViewer/Commands/ResizeWindowCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ImageViewer.Models;
+
+namespace ImageViewer.Commands
+{
+    public class ResizeWindowCommand : SimpleCommand
+    {
+        private readonly ModelsEx models;
+
+        public ResizeWindowCommand(ModelsEx models)
+        {
+            this.models = models;
+        }
+
+        public override void Execute()
+        {
+            var wnd = models.Window.Window;
+            // update settings
+            if (wnd.WindowState == WindowState.Maximized)
+            {
+                models.Settings.IsMaximized = true;
+            }
+            else if (wnd.WindowState != WindowState.Minimized)
+            {
+                models.Settings.IsMaximized = false;
+                models.Settings.WindowWidth = (int) wnd.Width;
+                models.Settings.WindowHeight = (int) wnd.Height;
+            }
+        }
+    }
+}
