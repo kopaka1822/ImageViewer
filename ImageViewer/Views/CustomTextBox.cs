48dbb866042a52c32bb32ec87b76b79fac31e988
Felix Brüll <felixbruell@live.de>
2019-10-05 20:25:20 +0200

    added old main window views

---
+++ ImageViewer/Views/CustomTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace ImageViewer.Views
+{
+    /// <summary>
+    /// text box that updates bindings when enter is pressed
+    /// </summary>
+    public class CustomTextBox : TextBox
+    {
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                base.OnKeyUp(e);
+                // prevent the key event from bubbling up
+                e.Handled = true;
+                return;
+            }
+
+            // update binding if enter was pressed
+            e.Handled = true;
+            var binding = BindingOperations.GetBindingExpression(this, TextProperty);
+            binding?.UpdateSource();
+            Keyboard.ClearFocus();
+        }
+    }
+}
