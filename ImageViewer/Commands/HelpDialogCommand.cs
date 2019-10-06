0b4a10b2b0f00c5c76852f90b2c486ae328e48fc
Felix Brüll <felixbruell@live.de>
2019-10-06 20:04:38 +0200

    added help dialogs

---
+++ ImageViewer/Commands/HelpDialogCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageViewer.Models;
+using ImageViewer.Views.Dialog;
+
+namespace ImageViewer.Commands
+{
+    public class HelpDialogCommand : SimpleCommand<string>
+    {
+        private readonly ModelsEx models;
+
+        public HelpDialogCommand(ModelsEx models)
+        {
+            this.models = models;
+        }
+
+        public override void Execute(string parameter)
+        {
+            var dia = new HelpDialog(models, parameter);
+
+            if(dia.IsValid)
+                models.Window.ShowDialog(dia);
+        }
+    }
+}
