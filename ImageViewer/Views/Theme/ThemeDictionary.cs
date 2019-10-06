c560e8ae5bbc2a6b074cc96418e47affab0ddadd
Felix <felixbruell@live.de>
2019-10-06 11:25:02 +0200

    started with themes

---
+++ ImageViewer/Views/Theme/ThemeDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ImageViewer.Properties;
+
+namespace ImageViewer.Views.Theme
+{
+    public class ThemeDictionary : ResourceDictionary
+    {
+        enum Themes
+        {
+            //Default,
+            White,
+            Dark,
+            Size
+        }
+
+        private Themes curTheme;
+
+        public ThemeDictionary()
+        {
+            var themeIdx = Settings.Default.Theme;
+            if (themeIdx < 0 || themeIdx >= (int) (Themes.Size))
+                themeIdx = 0;
+
+            curTheme = (Themes) themeIdx;
+        }
+
+        public Uri WhiteSource
+        {
+            set
+            {
+                if(curTheme == Themes.White)
+                    UpdateSource(value);
+            }
+        }
+
+        public Uri DarkSource
+        {
+            set
+            {
+                if(curTheme == Themes.Dark)
+                    UpdateSource(value);
+            }
+        }
+
+        private void UpdateSource(Uri theme)
+        {
+            base.Source = theme;
+        }
+    }
+}
