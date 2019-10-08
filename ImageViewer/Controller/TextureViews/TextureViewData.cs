3b07df9501134ae87f9943fe3a9288f9ae922c35
Felix Brüll <felixbruell@live.de>
2019-10-08 23:57:09 +0200

    added sampler to view data

--- ImageViewer/Controller/TextureViews/TextureViewData.cs
+++ ImageViewer/Controller/TextureViews/TextureViewData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SharpDX.Direct3D11;
+using SharpDX.Mathematics.Interop;
 
 namespace ImageViewer.Controller.TextureViews
 {
@@ -12,6 +13,9 @@ namespace ImageViewer.Controller.TextureViews
         public BlendState DefaultBlendState { get; }
         public BlendState AlphaBlendState { get; }
 
+        public SamplerState LinearSampler { get; }
+
+        public SamplerState PointSampler { get; }
         public TextureViewData()
         {
             var dev = ImageFramework.DirectX.Device.Get();
@@ -19,7 +23,8 @@ namespace ImageViewer.Controller.TextureViews
             DefaultBlendState = CreateBlendState(false, BlendOption.One, BlendOption.Zero);
             AlphaBlendState = CreateBlendState(true, BlendOption.SourceAlpha, BlendOption.InverseSourceAlpha);
 
-
+            LinearSampler = CreateSamplerState(true);
+            PointSampler = CreateSamplerState(false);
         }
 
         private static BlendState CreateBlendState(bool enable, BlendOption src, BlendOption dst)
@@ -45,10 +50,31 @@ namespace ImageViewer.Controller.TextureViews
             return new BlendState(dev.Handle, blendDesc);
         }
 
+        private static SamplerState CreateSamplerState(bool linear)
+        {
+            var dev = ImageFramework.DirectX.Device.Get();
+            var desc = new SamplerStateDescription
+            {
+                AddressU = TextureAddressMode.Wrap,
+                AddressV = TextureAddressMode.Wrap,
+                AddressW = TextureAddressMode.Wrap,
+                BorderColor = new RawColor4(),
+                ComparisonFunction = Comparison.Always,
+                Filter = linear ? Filter.MaximumMinMagMipLinear : Filter.ComparisonMinMagMipPoint,
+                MaximumAnisotropy = 0,
+                MaximumLod = float.MaxValue,
+                MinimumLod = 0,
+                MipLodBias = 0
+            };
+
+            return new SamplerState(dev.Handle, desc);
+        }
         public void Dispose()
         {
             DefaultBlendState?.Dispose();
             AlphaBlendState?.Dispose();
+            LinearSampler?.Dispose();
+            PointSampler?.Dispose();
         }
     }
 }
