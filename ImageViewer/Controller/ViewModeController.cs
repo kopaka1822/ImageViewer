71a4cac420ea44d091379080efd7a8d9189dabe0
Felix Brüll <felixbruell@live.de>
2019-10-08 21:07:21 +0200

    added correct paint function with split view

--- ImageViewer/Controller/ViewModeController.cs
+++ ImageViewer/Controller/ViewModeController.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using ImageViewer.Controller.TextureViews;
 using ImageViewer.Models;
 using SharpDX;
+using SharpDX.Direct3D11;
+using Device = ImageFramework.DirectX.Device;
 using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 using Point = System.Drawing.Point;
 
@@ -37,7 +39,62 @@ namespace ImageViewer.Controller
 
         public void Repaint()
         {
-            
+            var dev = Device.Get();
+            var size = models.Window.ClientSize;
+
+            var visible = models.GetEnabledPipelines();
+            if (visible.Count == 1)
+            {
+                // draw single image
+                currentView.Draw(models.Pipelines[visible[0]].Image);
+            }
+            else if (visible.Count == 2)
+            {
+                // draw two images in split view
+                var scissorsPos = new Point(mousePosition.X, mousePosition.Y);
+                scissorsPos.X = Math.Min(size.Width - 1, Math.Max(0, scissorsPos.X));
+                scissorsPos.Y = Math.Min(size.Height - 1, Math.Max(0, scissorsPos.Y));
+
+                if(models.Display.Split == DisplayModel.SplitMode.Vertical)
+                    dev.Rasterizer.SetScissorRectangle(0, 0, scissorsPos.X, size.Height);
+                else 
+                    dev.Rasterizer.SetScissorRectangle(0, 0, size.Width, scissorsPos.Y);
+
+                // first image
+                currentView.Draw(models.Pipelines[visible[0]].Image);
+
+                if (models.Display.Split == DisplayModel.SplitMode.Vertical)
+                    dev.Rasterizer.SetScissorRectangle(scissorsPos.X, 0, size.Width, size.Height);
+                else
+                    dev.Rasterizer.SetScissorRectangle(0, scissorsPos.Y, size.Width, size.Height);
+
+                // second image
+                currentView.Draw(models.Pipelines[visible[1]].Image);
+            }
+            else if (visible.Count == 3 || visible.Count == 4)
+            {
+                var scissorsPos = new Point(mousePosition.X, mousePosition.Y);
+                scissorsPos.X = Math.Min(size.Width - 1, Math.Max(0, scissorsPos.X));
+                scissorsPos.Y = Math.Min(size.Height - 1, Math.Max(0, scissorsPos.Y));
+
+                // upper left
+                dev.Rasterizer.SetScissorRectangle(0, 0, scissorsPos.X, scissorsPos.Y);
+                currentView.Draw(models.Pipelines[visible[0]].Image);
+
+                // upper right
+                dev.Rasterizer.SetScissorRectangle(scissorsPos.X, 0, size.Width, scissorsPos.Y);
+                currentView.Draw(models.Pipelines[visible[1]].Image);
+
+                // draw third texture (entire bottom if only 3 are visible, lower left if 4 are visible)
+                dev.Rasterizer.SetScissorRectangle(0, scissorsPos.Y, visible.Count == 3 ? size.Width : scissorsPos.X, scissorsPos.Y);
+                currentView.Draw(models.Pipelines[visible[2]].Image);
+
+                if(visible.Count == 4)
+                {
+                    dev.Rasterizer.SetScissorRectangle(scissorsPos.X, scissorsPos.Y, size.Width, size.Height);
+                    currentView.Draw(models.Pipelines[visible[3]].Image);
+                }
+            }
         }
 
         private void DxHostOnMouseMove(object sender, MouseEventArgs e)
