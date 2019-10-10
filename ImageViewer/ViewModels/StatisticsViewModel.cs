a33a3229a2c3f397bc4d41bdebd2df7df050f9b5
Felix <felixbruell@live.de>
2019-10-10 12:16:42 +0200

    added description for luminance luma etc.

--- ImageViewer/ViewModels/StatisticsViewModel.cs
+++ ImageViewer/ViewModels/StatisticsViewModel.cs
@@ -18,11 +18,27 @@ namespace ImageViewer.ViewModels
     {
         private readonly ModelsEx models;
         private readonly StatisticViewModel[] viewModels;
+        private static readonly string[] channelDescriptions = new[]
+        {
+            // luminance
+            @"Luminance is the radiant power weighted by a spectral sensitivity function that is characteristic of vision. 
+The magnitude of luminance is proportional to physical power. 
+But the spectral composition of luminance is related to the brightness sensitivity of human vision.
+Luminance is computed in linear color space with: dot(RGB, (0.2125, 0.7154, 0.0721)).",
+            // luma
+            @"Luma is brightness computed in sRGB color space which is often used by video codecs.
+The ""NTSC"" luma formula is: dot(sRGB, (0.299, 0.587, 0.114)).",
+            // lightness
+            @"Human vision has a nonlinear perceptual response to brightness: a source having a luminance only 18% of a reference luminance appears about half as bright. 
+The perceptual response to luminance Y is called lightness L.
+It is computed from the luminance: L = 116 * Y ^ (1/3) - 16."
+        };
 
         public StatisticsViewModel(ModelsEx models)
         {
             this.models = models;
             selectedChannel = AvailableChannels[(int)models.Settings.StatisticsChannel];
+            ChannelDescription = channelDescriptions[(int)models.Settings.StatisticsChannel];
 
             viewModels = new StatisticViewModel[models.NumPipelines];
             for (int i = 0; i < viewModels.Length; ++i)
@@ -55,6 +71,8 @@ namespace ImageViewer.ViewModels
             }
         }
 
+        public string ChannelDescription { get; private set; } = "";
+
         public List<ComboBoxItem<DefaultStatistics.Values>> AvailableChannels { get; } = new List<ComboBoxItem<DefaultStatistics.Values>>
         {
             new ComboBoxItem<DefaultStatistics.Values>("Luminance", DefaultStatistics.Values.Luminance),
@@ -70,7 +88,10 @@ namespace ImageViewer.ViewModels
             {
                 if (ReferenceEquals(value, selectedChannel)) return;
                 selectedChannel = value;
+                ChannelDescription = channelDescriptions[(int)value.Cargo];
                 OnPropertyChanged(nameof(SelectedChannel));
+                OnPropertyChanged(nameof(ChannelDescription));
+                models.Settings.StatisticsChannel = value.Cargo;
             }
         }
 
