b18683fa87b1810e54ab67ac4d1d78c2672ba12a
Felix Brüll <felixbruell@live.de>
2019-10-23 22:52:13 +0200

    added gif model for animated gif comparision

---
+++ ImageFramework/Model/Shader/GifShader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageFramework.DirectX;
+using SharpDX.Direct3D11;
+using Device = ImageFramework.DirectX.Device;
+
+namespace ImageFramework.Model.Shader
+{
+    public class GifShader : IDisposable
+    {
+        private struct BufferData
+        {
+            public int BorderLocation;
+            public int BorderSize;
+        }
+
+        private readonly QuadShader quad;
+        private readonly DirectX.Shader pixel;
+        private readonly UploadBuffer<BufferData> cbuffer;
+
+        public GifShader(QuadShader quad)
+        {
+            this.quad = quad;
+            pixel = new DirectX.Shader(DirectX.Shader.Type.Pixel, GetSource(), "GifPixelShader");
+            cbuffer = new UploadBuffer<BufferData>(1);
+        }
+
+        public void Run(ShaderResourceView left, ShaderResourceView right, RenderTargetView dst, int borderSize, int borderLocation, int width, int height)
+        {
+            Debug.Assert(borderLocation >= 0);
+            Debug.Assert(borderLocation < width);
+
+            var dev = Device.Get();
+            dev.Vertex.Set(quad.Vertex);
+            dev.Pixel.Set(pixel.Pixel);
+
+            // update buffer
+            cbuffer.SetData(new BufferData
+            {
+                BorderLocation = borderLocation,
+                BorderSize = borderSize
+            });
+
+            // bind and draw
+            dev.Pixel.SetConstantBuffer(0, cbuffer.Handle);
+            dev.Pixel.SetShaderResource(0, left);
+            dev.Pixel.SetShaderResource(1, right);
+            dev.OutputMerger.SetRenderTargets(dst);
+            dev.SetViewScissors(width, height);
+            dev.DrawQuad();
+
+            // unbind
+            dev.Pixel.SetShaderResource(0, null);
+            dev.Pixel.SetShaderResource(1, null);
+            dev.OutputMerger.SetRenderTargets((RenderTargetView)null);
+        }
+
+        private static string GetSource()
+        {
+            return @"
+Texture2D<float4> left : register(t0);
+Texture2D<float4> right : register(t1);
+
+cbuffer InfoBuffer : register(b0)
+{
+    int borderLocation;
+    int borderSize;
+};
+
+struct PixelIn
+{
+    float2 texcoord : TEXCOORD;
+    float4 projPos : SV_POSITION;
+};
+
+float4 main(PixelIn i) : SV_TARGET
+{
+    int2 coord = int2(i.projPos.xy);
+    float4 color;
+    if(coord.x > borderLocation) color = right[coord];
+    else color = left[coord];
+
+    if(abs(coord.x - borderLocation) < borderSize)
+        color = float4(0, 0, 0, 1);
+
+    return color;
+}
+";
+        }
+
+        public void Dispose()
+        {
+            pixel?.Dispose();
+        }
+    }
+}
