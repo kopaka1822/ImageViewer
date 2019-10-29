3b81d2eed877528984ce1934cce452394f4d6ed4
Felix Brüll <felixbruell@live.de>
2019-10-29 21:04:24 +0100

    added support for more compressed formats

--- DxImageLoader/compress_interface.cpp
+++ DxImageLoader/compress_interface.cpp
@@ -10,6 +10,9 @@ struct ExFormatInfo
 	bool useDxt1Alpha = false;
 	bool isCompressed = true;
 	bool swizzleRGB = false;
+	CMP_BYTE bx = 4;
+	CMP_BYTE by = 4;
+	CMP_BYTE bz = 1;
 	CMP_DWORD widthMultiplier = 0; // 0 for compressed formats. width multiplier to get pitch
 };
 
@@ -31,36 +34,76 @@ CMP_FORMAT get_cmp_format(gli::format format, ExFormatInfo& exInfo, bool isSourc
 		return CMP_FORMAT_RGBA_32F; // not supported by converter
 
 		// compressed formats
-	/*case gli::format::FORMAT_RGBA_ASTC_10X10_SRGB_BLOCK16:
+	case gli::format::FORMAT_RGBA_ASTC_10X10_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_10X10_UNORM_BLOCK16:
+		exInfo.bx = 10;
+		exInfo.by = 10;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_10X5_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_10X5_UNORM_BLOCK16:
+		exInfo.bx = 10;
+		exInfo.by = 5;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_10X6_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_10X6_UNORM_BLOCK16:
+		exInfo.bx = 10;
+		exInfo.by = 6;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_10X8_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_10X8_UNORM_BLOCK16:
+		exInfo.bx = 10;
+		exInfo.by = 8;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_12X10_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_12X10_UNORM_BLOCK16:
+		exInfo.bx = 12;
+		exInfo.by = 10;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_12X12_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_12X12_UNORM_BLOCK16:
+		exInfo.bx = 12;
+		exInfo.by = 12;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_4X4_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_4X4_UNORM_BLOCK16:
+		exInfo.bx = 4;
+		exInfo.by = 4;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_5X4_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_5X4_UNORM_BLOCK16:
+		exInfo.bx = 5;
+		exInfo.by = 4;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_5X5_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_5X5_UNORM_BLOCK16:
+		exInfo.bx = 5;
+		exInfo.by = 5;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_6X5_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_6X5_UNORM_BLOCK16:
+		exInfo.bx = 6;
+		exInfo.by = 5;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_6X6_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_6X6_UNORM_BLOCK16:
+		exInfo.bx = 6;
+		exInfo.by = 6;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_8X5_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_8X5_UNORM_BLOCK16:
+		exInfo.bx = 8;
+		exInfo.by = 5;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_8X6_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_8X6_UNORM_BLOCK16:
+		exInfo.bx = 8;
+		exInfo.by = 6;
+		return CMP_FORMAT_ASTC;
 	case gli::format::FORMAT_RGBA_ASTC_8X8_SRGB_BLOCK16:
 	case gli::format::FORMAT_RGBA_ASTC_8X8_UNORM_BLOCK16:
-		// TODO remember block dimensions
-		return CMP_FORMAT_ASTC;*/
+		exInfo.bx = 8;
+		exInfo.by = 8;
+		return CMP_FORMAT_ASTC;
 
 	case gli::format::FORMAT_RGB_DXT1_UNORM_BLOCK8: // BC 1
 	case gli::format::FORMAT_RGB_DXT1_SRGB_BLOCK8:
@@ -105,7 +148,7 @@ CMP_FORMAT get_cmp_format(gli::format format, ExFormatInfo& exInfo, bool isSourc
 	case gli::format::FORMAT_RGBA_BP_SRGB_BLOCK16:
 		return CMP_FORMAT_BC7;
 
-	/*case gli::format::FORMAT_RGB_ATC_UNORM_BLOCK8:
+	case gli::format::FORMAT_RGB_ATC_UNORM_BLOCK8:
 		return CMP_FORMAT_ATC_RGB;
 	case gli::format::FORMAT_RGBA_ATCI_UNORM_BLOCK16:
 		return CMP_FORMAT_ATC_RGBA_Interpolated;
@@ -113,18 +156,21 @@ CMP_FORMAT get_cmp_format(gli::format format, ExFormatInfo& exInfo, bool isSourc
 		return CMP_FORMAT_ATC_RGBA_Explicit;
 
 	case gli::format::FORMAT_RGB_ETC_UNORM_BLOCK8:
+		if (isSource) exInfo.swizzleRGB = true;
 		return CMP_FORMAT_ETC_RGB;
 
 	case gli::format::FORMAT_RGB_ETC2_SRGB_BLOCK8:
+		if (isSource) exInfo.swizzleRGB = true;
 		return CMP_FORMAT_ETC2_SRGB;
 	case gli::format::FORMAT_RGB_ETC2_UNORM_BLOCK8:
+		if (isSource) exInfo.swizzleRGB = true;
 		return CMP_FORMAT_ETC2_RGB;
-
 	case gli::format::FORMAT_RGBA_ETC2_SRGB_BLOCK8:
+		if (isSource) exInfo.swizzleRGB = true;
 		return CMP_FORMAT_ETC2_RGBA;
-
 	case gli::format::FORMAT_RGBA_ETC2_UNORM_BLOCK8:
-		return CMP_FORMAT_ETC2_SRGBA;*/
+		if (isSource) exInfo.swizzleRGB = true;
+		return CMP_FORMAT_ETC2_SRGBA;
 	}
 
 	exInfo.isCompressed = false;
@@ -156,9 +202,9 @@ void copy_level(uint8_t* srcDat, uint8_t* dstDat, uint32_t width, uint32_t heigh
 	srcTex.dwDataSize = CMP_DWORD(srcSize);
 	srcTex.format = srcFormat;
 	srcTex.pData = srcDat;
-	srcTex.nBlockWidth = 0;
-	srcTex.nBlockHeight = 0;
-	srcTex.nBlockDepth = 0;
+	srcTex.nBlockWidth = srcInfo.bx;
+	srcTex.nBlockHeight = srcInfo.by;
+	srcTex.nBlockDepth = srcInfo.bz;
 	if (!srcInfo.isCompressed && (srcInfo.swizzleRGB || dstInfo.swizzleRGB))
 		swizzleMipmap(srcDat, srcSize, srcFormat);
 
@@ -169,9 +215,9 @@ void copy_level(uint8_t* srcDat, uint8_t* dstDat, uint32_t width, uint32_t heigh
 	dstTex.dwHeight = height;
 	dstTex.dwPitch = dstInfo.widthMultiplier * dstTex.dwWidth;
 	dstTex.format = dstFormat;
-	dstTex.nBlockWidth = 0;
-	dstTex.nBlockHeight = 0;
-	dstTex.nBlockDepth = 0;
+	dstTex.nBlockWidth = dstInfo.bx;
+	dstTex.nBlockHeight = dstInfo.by;
+	dstTex.nBlockDepth = dstInfo.bz;
 	if (dstInfo.isCompressed) dstTex.dwDataSize = CMP_CalculateBufferSize(&dstTex);
 	else dstTex.dwDataSize = dstTex.dwPitch * dstTex.dwHeight;
 	//dst.bytes.resize(dstTex.dwDataSize);
@@ -214,10 +260,10 @@ void compressonator_convert_image(image::IImage& src, image::IImage& dst, int qu
 	const auto dstFormat = get_cmp_format(dst.getFormat(), dstFormatInfo, false);
 	const float fquality = quality / 100.0f;
 
-	for(size_t layer = 0; layer < src.getNumLayers(); ++layer)
+	for(uint32_t layer = 0; layer < src.getNumLayers(); ++layer)
 	{
 		// copy mipmap levels
-		for(size_t mipmap = 0; mipmap < src.getNumMipmaps(); ++mipmap)
+		for(uint32_t mipmap = 0; mipmap < src.getNumMipmaps(); ++mipmap)
 		{
 			uint32_t srcSize;
 			auto srcDat = src.getData(layer, mipmap, srcSize);
