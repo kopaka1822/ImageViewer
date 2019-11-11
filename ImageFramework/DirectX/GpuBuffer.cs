1872153588f65421d699b9ff4b23bda5f4b3e437
Felix Brüll <felixbruell@live.de>
2019-11-11 23:03:50 +0100

    added (untested) scan shader

---
+++ ImageFramework/DirectX/GpuBuffer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Buffer = SharpDX.Direct3D11.Buffer;
+
+namespace ImageFramework.DirectX
+{
+    /// <summary>
+    /// buffer in gpu memory with unordered access view
+    /// </summary>
+    public class GpuBuffer : IDisposable
+    {
+        public readonly Buffer Handle;
+        public readonly UnorderedAccessView View;
+        public int ElementSize { get; }
+        public int ElementCount { get; }
+        public int ByteSize { get; }
+
+        public GpuBuffer(int elementSize, int elementCount)
+        {
+            ElementSize = elementSize;
+            ElementCount = elementCount;
+            ByteSize = elementSize * elementCount;
+
+            var bufferDesc = new BufferDescription
+            {
+                BindFlags = BindFlags.UnorderedAccess,
+                CpuAccessFlags = CpuAccessFlags.None,
+                OptionFlags = ResourceOptionFlags.BufferStructured,
+                Usage = ResourceUsage.Default,
+                SizeInBytes = ByteSize,
+                StructureByteStride = elementSize
+            };
+
+            Handle = new Buffer(Device.Get().Handle, bufferDesc);
+            View = new UnorderedAccessView(Device.Get().Handle, Handle);
+        }
+
+        public void Dispose()
+        {
+            View?.Dispose();
+            Handle?.Dispose();
+        }
+    }
+}
