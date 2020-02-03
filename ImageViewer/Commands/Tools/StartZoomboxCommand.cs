21063dfde746b5d98d8a8759feb02bde73867ff5
Felix <felixbruell@live.de>
2020-02-03 16:38:00 +0100

    added dummy start command for zoombox

---
+++ ImageViewer/Commands/Tools/StartZoomboxCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageFramework.Model;
+using ImageViewer.Commands.Helper;
+using ImageViewer.Models;
+
+namespace ImageViewer.Commands.Tools
+{
+    public class StartZoomboxCommand : Command
+    {
+        private readonly ModelsEx models;
+
+        public StartZoomboxCommand(ModelsEx models)
+        {
+            this.models = models;
+            this.models.Images.PropertyChanged += ImagesOnPropertyChanged;
+        }
+
+        private void ImagesOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(ImagesModel.NumImages))
+                OnCanExecuteChanged();
+        }
+
+        public override bool CanExecute()
+        {
+            return models.Images.NumImages != 0;
+        }
+
+        public override void Execute()
+        {
+            
+        }
+    }
+}
