53be229ea2fc0175f0bd0f58e8f10c5d3f96f991
Felix <felixbruell@live.de>
2020-04-18 21:03:03 +0200

    cube skip shader optimizations

--- ImageViewer/Controller/TextureViews/Shader/CubeSkippingShader.cs
+++ ImageViewer/Controller/TextureViews/Shader/CubeSkippingShader.cs
@@ -32,6 +32,12 @@ namespace ImageViewer.Controller.TextureViews.Shader
             compute = new ImageFramework.DirectX.Shader(ImageFramework.DirectX.Shader.Type.Compute, GetSource(), "CubeSkippingShader");
         }
 
+        private struct BufferData
+        {
+            public Size3 Size;
+            public int Iteration;
+        }
+
         public void Run(ITexture src, ITexture dst, ITexture tmpTex, LayerMipmapSlice lm, UploadBuffer upload)
         {
             var size = src.Size.GetMip(lm.Mipmap);
@@ -42,17 +48,22 @@ namespace ImageViewer.Controller.TextureViews.Shader
             initTexShader.Run(src, dst, lm, upload);
             initTexShader.Run(src, tmpTex, lm, upload);
 
-            dev.Compute.Set(compute.Compute);
-            upload.SetData(size);
-            dev.Compute.SetConstantBuffer(0, upload.Handle);
-
             ImageFramework.DirectX.Query.SyncQuery syncQuery = new ImageFramework.DirectX.Query.SyncQuery();
-
+            //var watch = new Stopwatch();
+            //watch.Start();
             for (int i = 0; i < 254; ++i)
             {
                 // bind textures
                 dev.Compute.SetShaderResource(0, dst.GetSrView(lm));
                 dev.Compute.SetUnorderedAccessView(0, tmpTex.GetUaView(lm.Mipmap));
+
+                dev.Compute.Set(compute.Compute);
+                upload.SetData(new BufferData
+                {
+                    Size = size,
+                    Iteration = i
+                });
+                dev.Compute.SetConstantBuffer(0, upload.Handle);
 
                 // execute
                 dev.Dispatch(
@@ -71,7 +82,7 @@ namespace ImageViewer.Controller.TextureViews.Shader
                 dst = tmp;
 
 #if DEBUG
-                if (i % 8 == 0)
+                if (i % 16 == 0)
                 {
                     syncQuery.Set();
                     syncQuery.WaitForGpu();
@@ -80,6 +91,12 @@ namespace ImageViewer.Controller.TextureViews.Shader
 #endif
             }
 
+            /*syncQuery.Set();
+            syncQuery.WaitForGpu();
+            watch.Stop();
+
+            Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
+            */
             dev.Compute.Set(null);
 
             Debug.Assert(ReferenceEquals(originalDst, dst));
@@ -96,6 +113,7 @@ namespace ImageViewer.Controller.TextureViews.Shader
             return $@"
 cbuffer DirBuffer : register(b0){{
     int3 size;
+    uint iteration;
 }};
 
 Texture3D<uint> in_tex : register(t0);
@@ -104,7 +122,8 @@ RWTexture3D<uint> out_tex : register(u0);
 [numthreads({workgroupSize.X},{workgroupSize.Y},{workgroupSize.Z})]
 void main(int3 id : SV_DispatchThreadID) {{
     if(any(id >= size)) return; // outside
-    if(in_tex[id] == 0) return; // stays zero
+    if(in_tex[id] == 0) return; // remains zero    
+    if(in_tex[id] < iteration) return; // stays the same
 
     uint minVal = 255;
     
@@ -115,7 +134,9 @@ void main(int3 id : SV_DispatchThreadID) {{
         [flatten] if(id[i] + 1 < size[i]) minVal = min(minVal, in_tex[id + offset]);
     }}
 
-    out_tex[id] = min(minVal + 1, 255);
+    minVal = min(minVal + 1, 255);
+    //if(minVal != 255)
+        out_tex[id] = minVal;
 }}
 ";
         }
