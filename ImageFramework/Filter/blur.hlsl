a890ef5716fc40ee67f669da101e7bff665b8095
Felix <felixbruell@live.de>
2022-05-12 13:15:22 +0200

    added border handling to blur filter (clamp and repeat)

--- ImageFramework/Filter/blur.hlsl
+++ ImageFramework/Filter/blur.hlsl
@@ -7,6 +7,10 @@
 #param Variance, variance, float, 26, 1
 #param Blur Alpha, blur_alpha, bool, false
 
+#param Border Handling, border_handling, enum {Clamp; Repeat}, Clamp
+#define BORDER_CLAMP 0
+#define BORDER_REPEAT 1
+
 // Simple Gauss-Kernel. Normalization is not included and must be
 // done by dividing through the weight sum.
 float kernel(int _offset)
@@ -16,7 +20,11 @@ float kernel(int _offset)
 
 float4 getPixel(int3 pos, int3 size)
 {
-	pos = clamp(pos, 0, size-1);
+	if(border_handling == BORDER_CLAMP)
+		pos = clamp(pos, 0, size-1);
+	else if(border_handling == BORDER_REPEAT)
+		pos = int3(uint3(pos + blur_radius * size) % uint3(size)); // % is only well defined for positive numbers, since blur_radius > 1 and size > 1 the expression (pos + blur_radius * size) should be positive
+
 	return src_image[texel(pos)];
 }
 
