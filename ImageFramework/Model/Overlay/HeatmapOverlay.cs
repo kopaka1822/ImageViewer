e303f56b8e01bbed1100fbae0006e1143627da9b
Felix Brüll <felixbruell@live.de>
2022-05-29 15:58:59 +0200

    added shader for heatmap

--- ImageFramework/Model/Overlay/HeatmapOverlay.cs
+++ ImageFramework/Model/Overlay/HeatmapOverlay.cs
@@ -4,20 +4,124 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageFramework.DirectX;
 using ImageFramework.Utility;
+using SharpDX.Direct3D11;
 
 namespace ImageFramework.Model.Overlay
 {
     public class HeatmapOverlay : OverlayBase
     {
+        public enum Style
+        {
+            BlackRed,
+            //BlackRedYellowWhite,
+            BlackBlueGreenRed
+        }
+
+        private readonly ImagesModel images;
+        private readonly UploadBuffer cbuffer;
+        private UploadBuffer positionsBuffer;
+        private VertexBufferBinding vertexBufferBinding;
+        private HeatmapOverlayShader shader;
+        private HeatmapOverlayShader Shader => shader ?? (shader = new HeatmapOverlayShader());
+
+        public HeatmapOverlay(Models models)
+        {
+            this.cbuffer = models.SharedModel.Upload;
+            this.images = models.Images;
+        }
+
+        public struct Heatmap
+        {
+            // top left
+            public Float2 Start;
+            // bottom right
+            public Float2 End;
+            // fill style
+            public Style Style;
+            // border size
+            public int Border;
+            // optional text values (may be null)
+            public string MinText;
+            public string MaxText;
+        }
+
+        private Heatmap heatmap = new Heatmap();
+
+        public Heatmap Data
+        {
+            get => heatmap;
+            set
+            {
+                heatmap = value;
+                OnHasChanged();
+            }
+        }
+
         public override void Render(LayerMipmapSlice lm, Size3 size)
         {
             Debug.Assert(HasWork);
+
+            if (!IsEnabled) return;
+
+            UpdateData(lm.Mipmap);
+
+            // draw box
+            Shader.Bind(vertexBufferBinding);
+            Shader.Draw(heatmap, cbuffer, lm.Mipmap, size.XY);
+            Shader.Unbind();
+
+            // draw text
+            if (!String.IsNullOrEmpty(heatmap.MinText) || !String.IsNullOrEmpty(heatmap.MaxText))
+            {
+                //using(var d2d = new Direct2D(texture))
+            }
+            // TODO
+        }
+
+        private void UpdateData(int mipmap)
+        {
+            if (positionsBuffer == null)
+            {
+                // each box has 4 edges with 2 float positions
+                positionsBuffer = new UploadBuffer(2 * 4 * sizeof(float));
+                vertexBufferBinding = new VertexBufferBinding(positionsBuffer.Handle, 2 * sizeof(float), 0);
+            }
+
+            // fill buffer
+            var data = new float[2 * 4];
+            var dim = images.Size.GetMip(mipmap);
+            // offset float coordinates by half a unit so the quad covers the area
+            var offset = Size2.Zero.ToCoords(dim.XY);
+
+            // top left
+            data[0] = ToCanonical(heatmap.Start.X - offset.X);
+            data[1] = ToCanonical(heatmap.Start.Y - offset.Y);
+            // top right
+            data[2] = ToCanonical(heatmap.End.X + offset.X);
+            data[3] = ToCanonical(heatmap.Start.Y - offset.Y);
+            // bot left
+            data[4] = ToCanonical(heatmap.Start.X - offset.X);
+            data[5] = ToCanonical(heatmap.End.Y + offset.Y);
+            // bot right
+            data[6] = ToCanonical(heatmap.End.X + offset.X);
+            data[7] = ToCanonical(heatmap.End.Y + offset.Y);
+
+            // upload data
+            positionsBuffer.SetData(data);
+        }
+
+        // transform [0, 1] to [-1, 1]
+        private static float ToCanonical(float texcoord)
+        {
+            return texcoord * 2.0f - 1.0f;
         }
 
         public override void Dispose()
         {
-            
+            shader?.Dispose();
+            positionsBuffer?.Dispose();
         }
 
         private bool isEnabled = false;
