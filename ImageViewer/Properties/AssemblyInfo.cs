1c26f99ac7a9d064ffdccce57f97e5495c149f06
Felix Brüll <felixbruell@live.de>
2022-11-06 13:12:22 +0100

    fixed version to 3.6

--- ImageViewer/Properties/AssemblyInfo.cs
+++ ImageViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.*")]
+[assembly: AssemblyVersion("3.6.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
