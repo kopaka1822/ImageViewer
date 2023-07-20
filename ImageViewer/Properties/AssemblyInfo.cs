1569af1f8c2424dfb7937b5f424b28c22b8b2265
kopaka <felixbruelllive.de>
2023-07-20 10:24:54 +0200

    fixed version to 3.7

--- ImageViewer/Properties/AssemblyInfo.cs
+++ ImageViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.*")]
+[assembly: AssemblyVersion("3.7.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
