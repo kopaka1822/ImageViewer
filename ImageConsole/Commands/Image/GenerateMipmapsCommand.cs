335c83d96c859f427148c7bdb1cf9cba84788ea5
Felix <felixbruell@live.de>
2023-09-22 17:22:41 +0200

    added alpha scaling to mipmap generation

--- ImageConsole/Commands/Image/GenerateMipmapsCommand.cs
+++ ImageConsole/Commands/Image/GenerateMipmapsCommand.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ImageFramework.Model;
+using ImageFramework.Model.Scaling;
 
 namespace ImageConsole.Commands.Image
 {
     class GenerateMipmapsCommand : Command
     {
         public GenerateMipmapsCommand() 
-            : base("-genmipmaps", "", "(re)generates mipmaps")
+            : base("-genmipmaps", "[Box/Triangle/Lanczos/DetailPreserving/VeryDetailPreserving None/AlphaScale]", "(re)generates mipmaps. Default arguments are: Box None")
         {
         }
 
         public override void Execute(List<string> arguments, Models model)
         {
             var reader = new ParameterReader(arguments);
+
+            if(reader.HasMoreArgs())
+                model.Scaling.Minify = reader.ReadEnum<ScalingModel.MinifyFilters>("None/AlphaScale", ScalingModel.MinifyFilters.Box);
+
+            if (reader.HasMoreArgs())
+                model.Scaling.AlphaTestProcess =
+                    reader.ReadEnum<ScalingModel.AlphaTestPostprocess>("None/AlphaScale", ScalingModel.AlphaTestPostprocess.None);
+            
             reader.ExpectNoMoreArgs();
 
             if (model.Images.NumMipmaps > 1)
