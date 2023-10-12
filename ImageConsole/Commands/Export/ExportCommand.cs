56e77e1d3db5c0c94b93ba1626e335f6812760d0
Felix <felixbruell@live.de>
2023-10-12 12:17:26 +0200

    enhanced image console export command to automatically choose a format

--- ImageConsole/Commands/Export/ExportCommand.cs
+++ ImageConsole/Commands/Export/ExportCommand.cs
@@ -22,15 +22,14 @@ namespace ImageConsole.Commands.Export
         public int Mipmap { get; set; } = -1;
 
         public ExportCommand() 
-            : base("-export", "filename gliFormat", "saves the current image with the filename")
-        {
+            : base("-export", "filename (gliFormat)", "saves the current image with the filename") {
         }
 
         public override void Execute(List<string> arguments, Models model)
         {
             var reader = new ParameterReader(arguments);
             var filename = reader.ReadString("filename");
-            var format = reader.ReadEnum<GliFormat>("gliFormat");
+            var format = reader.ReadEnum<GliFormat>("gliFormat", GliFormat.UNDEFINED);
             reader.ExpectNoMoreArgs();
 
             // split filename
@@ -39,14 +38,51 @@ namespace ImageConsole.Commands.Export
                 throw new Exception($"{filename} missing file extension");
 
             var leftFile = filename.Substring(0, splitIdx);
-            var rightFile = filename.Substring(splitIdx + 1);
+            var extension = filename.Substring(splitIdx + 1);
 
             if (!model.Pipelines[0].IsValid)
                 throw new Exception("image formula is invalid.");
 
             // apply changes before exporting
             model.Apply();
-            var desc = new ExportDescription(model.Pipelines[0].Image, leftFile, rightFile)
+            var image = model.Pipelines[0].Image;
+
+            // handle automatic detection of export format
+            if (format == GliFormat.UNDEFINED)
+            {
+                // use original format of first image used in the equation
+                var firstId = model.Pipelines[0].Color.FirstImageId;
+                if(firstId >= 0 && firstId < model.Images.NumImages)
+                    format = model.Images.Images[firstId].OriginalFormat;
+
+                // test if this format is supported
+                var exportFormats = ExportDescription.GetExportFormat(extension);
+                if (exportFormats != null)
+                {
+                    if (!exportFormats.Formats.Contains(format))
+                    {
+                        // try to find a supported format:
+                        // find the closest matching format
+                        var bestScore = -float.MaxValue;
+                        var stats = model.Stats.GetStatisticsFor(image);
+                        
+                        foreach (var exformat in exportFormats.Formats)
+                        {
+                            if (image.Is3D && exformat.IsExcludedFrom3DExport()) continue;
+
+                            var rating = stats.GetFormatRating(exformat, GliFormat.UNDEFINED, false);
+                            if (rating > bestScore)
+                            {
+                                bestScore = rating;
+                                format = exformat;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            var desc = new ExportDescription(image, leftFile, extension)
             {
                 FileFormat = format,
                 UseCropping = UseCropping,
