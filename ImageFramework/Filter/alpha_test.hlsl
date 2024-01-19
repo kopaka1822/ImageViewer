77e7b5077a832bce7ea906fdc74d02c6152c1bbd
kopaka <felixbruelllive.de>
2024-01-19 12:54:36 +0100

    smoothen option in alpha test

--- ImageFramework/Filter/alpha_test.hlsl
+++ ImageFramework/Filter/alpha_test.hlsl
@@ -3,11 +3,23 @@
 #setting type, COLOR
 
 #param Threshold, t, float, 0.5, 0, 1
+#param Smoth with Alpha, smooth, bool, false
+
+#paramprop Threshold, onSubtract, -0.05, add
+#paramprop Threshold, onAdd, 0.05, add
+#keybinding Threshold, Add, 0.05, add
+#keybinding Threshold, OemPlus, 0.05, add
+#keybinding Threshold, Subtract, -0.05, add
+#keybinding Threshold, OemMinus, -0.05, add
 
 float4 filter(float4 color)
 {
 	float4 res = color;
 	if(color.a < t) res.a = 0.0;
-	else res.a = 1.0;
+	else if(smooth)
+	{
+		res.a = saturate((color.a - t) / (1.0 - t));
+	} 
+	else res.a = 1.0; // simply pass
 	return res;
 }
