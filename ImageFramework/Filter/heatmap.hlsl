a278dd272ca4eb1bc16a6966b63b7074bbcdfee9
Felix <felixbruell@live.de>
2024-05-02 17:47:23 +0200

    added more heatmap modi (viridis and ct scan)

--- ImageFramework/Filter/heatmap.hlsl
+++ ImageFramework/Filter/heatmap.hlsl
@@ -2,7 +2,7 @@
 #setting description, Maps the red input channel to a color map. Values outside [Min Value, Max Value] are clamped. A screen overlay can be enabled via Overlays->Heatmap.
 #setting type, COLOR
 
-#param Type, type, enum { Grayscale; Inferno; CoolWarm; BlackBody }, Inferno
+#param Type, type, enum { Grayscale; Inferno; CoolWarm; BlackBody; Viridis; Kindlmann; CTScan }, Inferno
 
 #param Min Value, minVal, float, 0.0
 #param Max Value, maxVal, float, 1.0
@@ -58,6 +58,41 @@ static float4 blackBody[] = {
     float4(255, 255, 255, 1),
 };
 
+static float4 viridis[] = {
+    float4(68, 1, 84, 0),
+    float4(70, 50, 127, 0.142857143),
+    float4(54, 92, 141, 0.285714286),
+    float4(39, 127, 142, 0.428571429),
+    float4(31, 161, 135, 0.571428571),
+    float4(74, 194, 109, 0.714285714),
+    float4(159, 218, 58, 0.857142857),
+    float4(253, 231, 37, 1),
+};
+
+static float4 kindlmann[] = {
+    float4(0, 0, 0, 0),
+    float4(36, 6, 117, 0.142857143),
+    float4(7, 62, 150, 0.285714286),
+    float4(5, 115, 97, 0.428571429),
+    float4(8, 159, 21, 0.571428571),
+    float4(112, 196, 9, 0.714285714),
+    float4(250, 208, 146, 0.857142857),
+    float4(255, 255, 255, 1),
+};
+
+// values for normalized range from -1000 to 1000
+// from Automatic Perceptual Color Map Generation for Realistic Volume Visualization (2008)
+static float4 CT[] = {
+    float4(0, 0, 0, 0), // -1000 air filled vaity
+    float4(194, 105, 82, 0.2), // -600 lung parenchyma
+    float4(194, 105, 82, 0.3), // -400 ''
+    float4(194, 166, 115, 0.45), // -100 fat
+    float4(194, 166, 115, 0.47), // -60''
+    float4(102, 0, 0, 0.52), // 40 soft tissue
+    float4(153, 0, 0, 0.54), // 80 ''
+    float4(255, 255, 255, 0.7) // 400+ bone
+};
+
 
 #define COLOR_TABLE_FUNC(SIZE)                            \
 float3 getColorFromTable##SIZE(float v, float4 table[SIZE]) {   \
@@ -82,7 +117,7 @@ float3 getColorGray(float v)
 float4 filter(float4 incolor)
 {
 	float v = max(0,min(1,(incolor.x - minVal) / (maxVal - minVal)));
-	float4 color = float4(0.0, 0.0, 0.0, 1.0);
+	float4 color = float4(0.0, 0.0, 0.0, incolor.a);
 
 	switch(type)
 	{
@@ -97,6 +132,15 @@ float4 filter(float4 incolor)
         break;
     case 3:
         color.xyz = getColorFromTable8(v, blackBody);
+        break;
+    case 4:
+        color.xyz = getColorFromTable8(v, viridis);
+        break;
+    case 5:
+        color.xyz = getColorFromTable8(v, kindlmann);
+        break;
+    case 6:
+        color.xyz = getColorFromTable8(v, CT);
         break;
 	}
 	
