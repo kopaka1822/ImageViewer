31cd06f8b13eb918d64f90827ded64ab5a65cbda
kopaka1822 <felixbruell@live.de>
2026-03-26 21:01:27 +0100

    updated version in assembly

--- ImageViewer/Properties/AssemblyInfo.cs
+++ ImageViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageViewer")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.0")]
+[assembly: AssemblyVersion("3.9.*")]
 [assembly: AssemblyFileVersion("3.0.0")]
